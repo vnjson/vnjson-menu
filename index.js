2a4046dd476a21087e8ce2e26f6a1dc39f265b25
bakakasin <bakakasin@gmail.com>
2017-06-07 02:17:45 +0700

    append menuitem

--- index.js
+++ index.js
@@ -1,46 +1,94 @@
 /*
- * {
- *    'Пункт меню 1': 'label1',
- *    'Пункт меню 2': { label: 'label2', data: 'true'},
- *    'Пункт меню 3': 'label3',
- *    'Пункт меню 4': 'label4'
- * }
- * ('mousedown',(e)=>{e.target.id})
-
+ * [bug] не коррестно получает ctx.screen
+ * @config { String } mainScreen __hack__
+ * @config { String } textBox // Use it for .off('mousedown')
+ * @config { String } menuItemClass
+ * @emit('data', vnjs.ctx.data, {points: +points})
  */
 
 vnjs.on('menu', function(data){
-  let { config, ctx, parse } = this;
+  let { config, ctx, parse, next, emit } = this;
 
-  var fragment = document.createDocumentFragment();
-  var elemMenu = document.createElement('div');
-      elemMenu.className=config.screenPrefix+"menu";
-      
-    for(let key in data){
-      let menuItem = document.createElement('div');
-          menuItem.innerHTML = key;      
-           /* if(typeof data[key]==='object'){
-                menuItem.setAttribute('data-label',data[key].label);
-                vnjs.ctx.data = data[key].data;
-                /*
-                   ctx.choises = [
-                      {label,scene,num, data}
-                  ]
-               
-            }else if(typeof data[key]==='string'){
-           */
-                menuItem.setAttribute('data-label',data[key]);
-           // }
-          menuItem.className = config.screenPrefix+"menu-item";
-      elemMenu.appendChild(menuItem);   
-         
-    };
+  const wrapperClassName = config.prefix+"memu";
+  const subMenu = `#${config.prefix}${config.mainScreen}`;
 
-    ctx.screen.appendChild(elemMenu);
-    var elMenu = document.querySelector('.'+config.screenPrefix+"menu");
-        elMenu.addEventListener('mousedown', function(e){
-          let label = e.target.getAttribute('data-label');
+$(config.textBox).off('mousedown');
+/*
+ * Удаляю слой меню
+ * Вешаю событие click
+ */
+function resume(){
+    $("."+wrapperClassName).remove();
+    $(config.textBox).on('mousedown', next);
+};
 
-            parse(`jump: ${label}`);
-        })
+var fragment = document.createDocumentFragment();
+var elemMenu = document.createElement('div');
+    elemMenu.className = wrapperClassName;
+
+var val, menuItem;
+/*
+ * Пробегаюсь по меню {menuItem1, menuItem2}
+ * И создаю пункты меню в DOM
+ */
+for(let key in data){
+    val = data[key];
+    if(typeof val==='object'){
+        menuItem = `<li><a href="#" data-points="${val.points}" data-jump="${val.jump}" class="${config.menuItemClass}">${key}</a></li>`;
+      $(elemMenu).append(menuItem);
+
+    }else if(typeof val==='string'){
+       
+        menuItem = `<li><a href="#" data-jump="${val}" class="${config.menuItemClass}">${key}</a></li>`;
+        $(elemMenu).append(menuItem);
+    }
+}
+
+fragment.appendChild(elemMenu);
+
+document
+  .querySelector(subMenu)
+  .appendChild(fragment); 
+/*
+ * Делегирование событий. Пункты меню
+ * @attr data-jump
+ * @attr data-points
+ */
+$('.'+wrapperClassName).on('mousedown', function(e){
+        e.preventDefault();
+        let label = $(e.target).attr('data-jump');
+        let points = $(e.target).attr('data-points');
+        let key = $(e.target).text();
+        /*
+         * Проверка существование points
+         */
+        if(points!==undefined){
+
+           vnjs.ctx.data.points+=+points;
+           emit('data', vnjs.ctx.data, {key: key, points: +points})
+        }
+        /*
+         * Проверка на существование label
+         *
+         */
+        if(label.length<=1){
+            next();
+            resume();
+        }else if(typeof label ===undefined){
+            resume();
+        }else{
+            resume();
+            vnjs.parse({'jump': label});
+
+        }
+
+});
+
+ 
+});
+
+
+vnjs.on('data',function(data, newData){
+  console.log("points: "+data.points);
+  //console.log(newData)
 });
